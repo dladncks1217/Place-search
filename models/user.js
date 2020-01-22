a5d8991a9a1106092524879566544a397f412e8a
dladncks1217 <dlaxodud1217@gmail.com>
2020-01-22 22:54:58 +0900

    [develop] #18 Database Bugfix (user is not associated to user -> Add primaryKey)

--- models/user.js
+++ models/user.js
@@ -4,15 +4,16 @@ module.exports = (sequelize,DataTypes)=>(
             type:DataTypes.STRING(40),
             allowNull:false,
             unique:true,
+            primaryKey:true,
         },
         password:{
             type:DataTypes.STRING(100),
             allowNull:false,
-            unique:true,
         },
         nick:{
             type:DataTypes.STRING(15),
             allowNull:true,
+            unique:true,
         }
     })
 );
