102a0e2c112e79bbdb069abaa0bcb95e9f2e8c95
dladncks1217 <dlaxodud1217@gmail.com>
2020-01-22 18:02:02 +0900

    [develop] #10 #18 #23 Bugfix routes/index.js (Not confirmed)

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,25 @@
 const express = require('express');
+const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
 const router = express.Router();
 
 require('dotenv').config();
 
+/*
+const isLoggedin = (req,res,next)=>{
+    if (req.isAuthenticated()) {
+        req.isLogged = true
+        return next();
+     }
+     res.redirect('/');
+}
+*/
+
 router.get('/',(req,res,next)=>{
     res.render('index',{
-        KAKAO_JAVASCRIPTKEY : process.env.KAKAO_JAVASCRIPTKEY,
-    });
+        isLoggedIn:req.isAuthenticated(),
+        user:req.user,
+    })
+    
 });
 
 module.exports = router;
