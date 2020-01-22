4c41070974b9423c3f3e86b1b83ea18be0537f45
dladncks1217 <dlaxodud1217@gmail.com>
2020-01-22 22:57:19 +0900

    [develop] #18 Ejs Rendering Bugfix

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
 const router = express.Router();
+const {User} = require('../models');
 
 require('dotenv').config();
 
@@ -14,11 +15,19 @@ const isLoggedin = (req,res,next)=>{
 }
 */
 
-router.get('/',(req,res,next)=>{
-    res.render('index',{
-        isLoggedIn:req.isAuthenticated(),
-        user:req.user,
-    })
+router.get('/',async(req,res,next)=>{
+    if(req.isAuthenticated()){
+        const user = await User.findOne({where:{email: req.user.email}});
+        res.render('index',{
+            isLoggedIn:req.isAuthenticated(),
+            user:user.nick,
+        });
+    }else{
+        res.render('index',{
+            isLoggedIn:req.isAuthenticated(),
+        });
+    }
+    
     
 });
 
