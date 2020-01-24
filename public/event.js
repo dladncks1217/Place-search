733ed6690d7cff9831e850ab0a154dc60d946515
dladncks1217 <dlaxodud1217@gmail.com>
2020-01-24 16:18:15 +0900

    [develop] #2 Send JSON data to /history (Add time)

--- public/event.js
+++ public/event.js
@@ -1,9 +1,20 @@
+function timecheck(){
+    let now = new Date();
+    let years = now.getFullYear();
+    let months = now.getMonth()+1;
+    let date = now.getDate();
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    
+    return `${years}년 ${months}월 ${date}일 ${hours}시 ${minutes}분`;
+}
 document.querySelector('#search').focus();
 document.getElementById('searchbtn').addEventListener('click', (e) => {
     e.preventDefault();
     searchfeature();
     document.getElementById('srchresult').textContent = document.getElementById('search').value;
     document.getElementById('search').value="";
+
 
     let xhr = new XMLHttpRequest();
     xhr.onload = ()=>{
@@ -15,7 +26,7 @@ document.getElementById('searchbtn').addEventListener('click', (e) => {
     };
     xhr.open('POST','/history');
     xhr.setRequestHeader('Content-Type','application/json');
-    xhr.send(JSON.stringify({history:document.getElementById('srchresult').textContent}));
+    xhr.send(JSON.stringify({history:document.getElementById('srchresult').textContent, time:timecheck()}));
 });
 
 function enterkey(e) {
@@ -34,7 +45,7 @@ function enterkey(e) {
         };
         xhr.open('POST','/history');
         xhr.setRequestHeader('Content-Type','application/json');
-        xhr.send(JSON.stringify({history:document.getElementById('srchresult').textContent}));
+        xhr.send(JSON.stringify({history:document.getElementById('srchresult').textContent, time:timecheck()}));
     }
 }
 if(document.getElementById('login')){
