95ac87c3268a1f7130bb0db35ca661f3c32fd527
dladncks1217 <dlaxodud1217@gmail.com>
2020-01-26 12:29:34 +0900

    [develop] #2 #23 Add History save feature, Map front bug

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,31 @@
 const express = require('express');
-const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
 const router = express.Router();
-const {User} = require('../models');
+const {User,History} = require('../models');
 
 require('dotenv').config();
 
 router.get('/',async(req,res,next)=>{
-    if(req.isAuthenticated()){
-        const user = await User.findOne({where:{email: req.user.email}});
-        res.render('index',{
-            isLoggedIn:req.isAuthenticated(),
-            user:user.nick,
-        });
-    }else{
-        res.render('index',{
-            isLoggedIn:req.isAuthenticated(),
-        });
+    try{
+        if(req.isAuthenticated()){
+            const user = await User.findOne({where:{email: req.user.email}});
+            await History.findAndCountAll({where:{userId:user.id}})
+            .then((history)=>{
+                res.render('index',{
+                    isLoggedIn:req.isAuthenticated(),
+                    user:user.nick,
+                    history,
+                    count:history.count,
+                });
+            });
+        }else{
+            res.render('index',{
+                isLoggedIn:req.isAuthenticated(),
+            });
+        }
+    }catch(err){
+        console.error(err);
+        next(err);
     }
-    
-    
 });
 
 module.exports = router;
