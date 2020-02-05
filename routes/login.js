2545e1e815f2e3c73f10906010d6957fb05bef96
dladncks1217 <dlaxodud1217@gmail.com>
2020-02-06 04:20:46 +0900

    [develop] #2 #52 #54 Add Mailsend Router (wontfix)

--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const nodemailer = require('nodemailer');
 const router = express.Router();
 const {isLoggedIn, isNotLoggedIn} = require('./middlewares');
 const passport = require('passport');
 const { User } = require('../models');
+
+require('dotenv').config();
 
 router.get('/',(req,res,next)=>{
     res.render('login');
@@ -19,8 +22,6 @@ router.post('/login',isNotLoggedIn,(req,res,next)=>{
         }
         return req.login(user,(loginError)=>{
             if(loginError){
-                console.log(user);
-                console.log('여기서에러났어요');
                 console.error(loginError);
                 return next(loginError);
             }
@@ -39,7 +40,28 @@ router.post('/findpw/find',isNotLoggedIn, async(req,res,next)=>{
         req.flash('EmailNotfound','이메일을 찾을 수 없습니다!')
         return res.redirect('/login/findpw');
     }else{
-        
+        let transporter = nodemailer.createTransport({
+            service: 'Naver',
+            auth: {
+              user: process.env.NAVER_ID,  
+              pass: process.env.NAVER_PASSWORD,  
+            }
+        });
+        let mailOptions = {
+            from: process.env.NAVER_ID,    // 발송 메일 주소 
+            to: exUser.email,                     // 수신 메일 주소
+            subject: '새로운 비밀번호입니다.',   // 제목
+            text: 'That was easy!'  // 내용
+          };
+        transporter.sendMail(mailOptions, (error, info)=>{
+            if (error) {
+                console.log(error);
+            }
+            else {
+                console.log('Email sent: ' + info.response);
+            }
+            return res.redirect('/');
+        }); 
     }
 });
 
