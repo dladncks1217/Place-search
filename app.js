65c53153692f445d469298f5513d84001892a730
dladncks1217 <dlaxodud1217@gmail.com>
2021-08-10 15:41:11 +0900

    [master] #73 connect Redis

--- app.js
+++ app.js
@@ -4,10 +4,20 @@ const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const flash = require('connect-flash');
+const hpp = require('hpp');
+const helmet = require('helmet');
+const redis = require('redis');
 const passport = require('passport');
+const RedisStore = require('connect-redis')(session);
 const passportConfig = require('./passport');
 
 require('dotenv').config();
+
+const redisClient = redis.createClient({
+    url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+    password: process.env.REDIS_PASSWORD,
+  });
+
 
 const indexRouter = require('./routes/index');
 const loginRouter = require('./routes/login');
@@ -25,22 +35,37 @@ passportConfig(passport);
 
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine','ejs');
-app.set('port',process.env.PORT||8015);
+app.set('port',process.env.PORT||8000);
 
-app.use(morgan('dev'));
+if(process.env.NODE_ENV === "production"){
+    app.use(morgan('combined'));
+    app.use(helmet());
+    app.use(hpp());
+}else{
+    app.use(morgan('dev'));
+}
 app.use(express.static(path.join(__dirname,'public')));
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(session({   
-    resave:false,
-    saveUninitialized:false,
+const sessionOption = {
+    resave: false,
+    saveUnitialized:false,
     secret:process.env.COOKIE_SECRET,
     cookie:{
         httpOnly:true,
         secure:false,
     },
-}));
+
+    store: new RedisStore({
+        client:redisClient
+    })
+}
+if(process.env.NODE_ENV === 'production'){
+    sessionOption.proxy=true;    
+    // sessionOption.cookie.secure=true;
+}
+app.use(session(sessionOption));
 app.use(flash());
 
 //passport
--- config/config.js
+++ config/config.js
@@ -2,27 +2,27 @@ require('dotenv').config();
 
 module.exports = {
   "development": {
-    "username": "root",
-    "password": process.env.DATABASE_PASSWORD,
-    "database": "nodeplace",
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
     "host": "127.0.0.1",
     "dialect": "mysql",
     "operatorsAliases": false
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
     "host": "127.0.0.1",
     "dialect": "mysql",
-    "operatorsAliases": false
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
     "host": "127.0.0.1",
     "dialect": "mysql",
-    "operatorsAliases": false
+    "operatorsAliases": false,
+    "logging":false,
   }
 }
